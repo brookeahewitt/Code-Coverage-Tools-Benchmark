dfccc0178a6318a46eda5dd6674c25ce4dce84e8
Brooke Hewitt <112017131+brookeahewitt@users.noreply.github.com>
2025-04-12 12:36:53 -0400

    Added loops test case

--- tests/loops.c
+++ tests/loops.c
@@ -6,7 +6,20 @@
 #include <klee/klee.h>
 
 int loops(int x) {
-    // Find sum
+    int *squares = malloc(sizeof(int) * (x + 1));
+
+    for (int i = 0; i <= x; i++) {
+        squares[i] = i*i;
+    }
+
+    int sum = 0;
+    int i = 0;
+    while (i <= x) {
+        sum += squares[i];
+        i++;
+    }
+
+    return sum;
 }
 
 int main(int argc, char** argv) {
@@ -17,11 +30,14 @@ int main(int argc, char** argv) {
         klee_make_symbolic(&x, sizeof(x), "x");
     } else if (argc == 2) { // FOR AFL
         x = atoi(argv[1]);
+        if (x < 0) {
+            x *= -1;
+        }
     } else {
         fprintf(stderr, "Usage: %s <int x>\n", argv[0]);
         return 1;
     }    
 
-    loops(x);
+    printf("Sum of Squares from 0 to %d: %d", x, loops(x));
     return 0;
 }
