5732939b9339b80344d78b3da5fda0c33eec0c9f
Brooke Hewitt <112017131+brookeahewitt@users.noreply.github.com>
2025-04-12 14:43:35 -0400

    Added test case for conditional logic

---
+++ tests/conditional_logic.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+#include <string.h>
+#include <klee/klee.h>
+
+void calendar(int day, int month) {
+    switch (day) {
+        case 1:
+            printf("Sunday\n");
+            break;
+        case 2:
+            printf("Monday\n");
+            break;
+        case 3:
+            printf("Tuesday\n");
+            break;
+        case 4:
+            printf("Wednesday\n");
+            break;
+        case 5:
+            printf("Thursday\n");
+            break;
+        case 6:
+            printf("Friday\n");
+            break;
+        case 7:
+            printf("Saturday\n");
+            break;
+        default:
+            printf("Invalid day of the week\n");
+            break;
+    }
+
+    if (month == 1) {
+        printf("January\n");
+    } else if (month == 2) {
+        printf("February\n");
+    } else if (month == 3) {
+        printf("March\n");
+    } else if (month == 4) {
+        printf("April\n");
+    } else if (month == 5) {
+        printf("May\n");
+    } else if (month == 6) {
+        printf("June\n");
+    } else if (month == 7) {
+        printf("July\n");
+    } else if (month == 8) {
+        printf("August\n");
+    } else if (month == 9) {
+        printf("September\n");
+    } else if (month == 10) {
+        printf("October\n");
+    } else if (month == 11) {
+        printf("November\n");
+    } else if (month == 12) {
+        printf("December\n");
+    } else {
+        printf("Invalid month\n");
+    }
+}
+
+int main(int argc, char** argv) {
+    int x, y;
+
+    // FOR KLEE
+    if (argc > 1 && strcmp(argv[1], "-k") == 0) {
+        klee_make_symbolic(&x, sizeof(x), "x");
+        klee_make_symbolic(&y, sizeof(y), "y");
+    } else if (argc == 3) { // FOR AFL
+        x = atoi(argv[1]);
+        y = atoi(argv[2]);
+    } else {
+        fprintf(stderr, "Usage: %s <int x> <int y>\n", argv[0]);
+        return 1;
+    }    
+
+    calendar(x, y);
+    return 0;
+}
