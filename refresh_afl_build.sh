e2456c7954a55a5209d3863e224092f3918cad51
Brooke Hewitt <dnj6xk@virginia.edu>
2025-04-15 13:13:11 -0400

    Added script to compile files for AFL++

---
+++ refresh_afl_build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Set paths
+TEST_DIR=~/Code-Coverage-Tools-Benchmark/tests
+BUILD_DIR=~/Code-Coverage-Tools-Benchmark/afl_build
+
+# Create build directory if it doesn't exist
+mkdir -p "$BUILD_DIR"
+
+# Loop over all .c files in the test directory
+for src_file in "$TEST_DIR"/*.c; do
+    # Get the base filename without directory or extension
+    base_name=$(basename "$src_file" .c)
+    
+    # Construct the output filename
+    output_file="${BUILD_DIR}/${base_name}_afl"
+
+    # Compile with coverage flags and output the binary
+    gcc -fprofile-arcs -ftest-coverage -O0 -o "$output_file" "$src_file" -lm
+    
+    echo "Compiled $src_file -> $output_file"
+done
